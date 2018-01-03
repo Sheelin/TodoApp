53dbe994c4512d24dd06dee623a4e15f29e6bfe9
Fiona <finara.k@hotmail.com>
2018-01-03 23:59:03 +0100

    change webpack to alias components automatically

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,45 @@
 var webpack = require('webpack');
 
 module.exports = {
-    entry: [
-        'script!jquery/dist/jquery.min.js',
-        './app/app.jsx'
+  entry: [
+    'script!jquery/dist/jquery.min.js',
+    './app/app.jsx'
+  ],
+  externals: {
+    jquery: 'jQuery'
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      '$': 'jquery',
+      'jQuery': 'jquery'
+    })
+  ],
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  resolve: {
+    root: __dirname,
+    modulesDirectories: [
+      'node_modules',
+      './app/components'
     ],
-    externals: {
-        jquery: 'jQuery'
+    alias: {
+      applicationStyles: 'app/styles/app.scss',
     },
-    plugins: [
-        new webpack.ProvidePlugin({
-            '$': 'jquery',
-            'jQuery': 'jquery'
-        })
-    ],
-    output: {
-        path: __dirname,
-        filename: './public/bundle.js'
-    },
-    resolve: {
-        root: __dirname,
-        alias: {
-            applicationStyles: 'app/styles/app.scss',
-            Main: 'app/components/Main.jsx',
-            MenuItem2: 'app/components/MenuItem2.jsx',
-            TodoApp: 'app/components/TodoApp.jsx',
-            Navigation: 'app/components/Navigation.jsx'
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
         },
-        extensions: ['', '.js', '.jsx']
-    },
-    module: {
-        loaders: [
-            {
-                loader: 'babel-loader',
-                query: {
-                    presets: ['react', 'es2015']
-                },
-                test: /\.jsx?$/,
-                exclude: /(node_modules|bower_components)/
-            }
-        ]
-    },
-    devtool: 'cheap-module-eval-source-map'
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/
+      }
+    ]
+  },
+  devtool: 'cheap-module-eval-source-map'
 };
