aa7475422b182cb986905864ba77048ee99c0c99
Fiona <finara.k@hotmail.com>
2018-01-04 01:25:12 +0100

    fix up tests

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ var path = require('path');
 module.exports = {
   entry: [
     'script!jquery/dist/jquery.min.js',
+    'script!foundation-sites/dist/foundation.min.js',
     './app/app.jsx'
   ],
   externals: {
@@ -41,6 +42,11 @@ module.exports = {
         test: /\.jsx?$/,
         exclude: /(node_modules|bower_components)/
       }
+    ]
+  },
+  sassLoader: {
+    includePaths: [
+      path.resolve(__dirname, './node_modules/foundation-sites/scss')
     ]
   },
   devtool: 'cheap-module-eval-source-map'
